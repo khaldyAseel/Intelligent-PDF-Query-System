ee8b0ca9181f5a5ea541b490487f2d3ca49e1e7c
abdallasal <abdallasalamah99@gmail.com>
2025-02-24 20:05:18 +0200

    changes

--- backend/models/agent1.py
+++ backend/models/agent1.py
@@ -8,6 +8,7 @@ import os
 from dotenv import load_dotenv
 from together import Together
 
+from llama_index.core.schema import Document
 from llama_index.core import SimpleDirectoryReader
 from llama_index.retrievers.bm25 import BM25Retriever
 from llama_index.core.node_parser import SentenceSplitter
@@ -19,17 +20,28 @@ api_key = os.getenv("TOGETHER_API_KEY")
 client = Together(api_key=api_key)
 
 #top k nodes parameter
-K=10
+K=5
 
-file_path = r""
-documents = SimpleDirectoryReader(file_path).load_data()
-print(len(documents))
 
-splitter = SentenceSplitter(chunk_size=500, chunk_overlap=50)
-nodes = splitter.get_nodes_from_documents(documents)
-print(len(nodes))
-print(nodes[0].metadata)
-print(nodes[0].text)
+#text data as a parameter, replace with the text from the database (the sub chapter)
+#also replace the metadata from the database
+text = ""
+metadata = {"page_number": 5}
+
+#convert to a Document object
+document = Document(
+    text=text,
+    metadata=metadata
+)
+
+splitter = SentenceSplitter(chunk_size=500, chunk_overlap=50)  # Adjust chunk size as needed
+nodes = splitter.get_nodes_from_documents([document])
+
+# Print nodes
+for node in nodes:
+    print(node.text)
+    print(node.metadata)
+
 
 
 question = "What factors contribute to price volatility in the cocoa market?"
@@ -51,19 +63,18 @@ context_lst = []
 for i in range(K):
     context_lst.append(bm25_retrieved_nodes[i].text)
 
-responses = []  # List to store responses
+#using the retrieved nodes as the context
+context_lst = []
+for i in range(K):
+    context_lst.append(bm25_retrieved_nodes[i].text)
 
-for context in context_lst:
-    response = client.chat.completions.create(
-        model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
-        messages=[
-            {"role": "system", "content": "You are a helpful chatbot."},
-            {"role": "user", "content": f"Answer the question: {query}. Use only information provided here: {context}"},
-        ],
-    )
+all_context = "\n".join(context_lst)
 
-    responses.append(response.choices[0].message.content)  # Store each response
-
-# Print all responses
-for i, resp in enumerate(responses):
-    print(f"Response {i + 1}: {resp}\n")
+response = client.chat.completions.create(
+    model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+    messages=[
+      {"role": "system", "content": "You are a helpful chatbot."},
+      {"role": "user", "content": f"Answer the question: {query}. Use only information provided here: {all_context}"},
+    ],
+)
+print(response.choices[0].message.content)
