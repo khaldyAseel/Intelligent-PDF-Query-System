8491b2c7bf7d7779b7fabf851d61183777fc23a5
cleopypavlou <cleopy.8.7@gmail.com>
2025-03-25 21:41:17 +0200

    logo added

--- frontend/src/components/HistoryComponent.jsx
+++ frontend/src/components/HistoryComponent.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "/Users/cleopypavlou/Desktop/aka.svg";
 import './historyStyle.css'
 
 export const HistoryComponent = ({ history, onHistoryClick }) => {
   return (
     <div className="history-container">
-        <img src={logo} alt="Logo" className="logo" />
+        <img src="/aka.svg" alt="Logo" className="logo" />
+
 
       <ul>
         {history.map((query, index) => (
@@ -15,9 +15,11 @@ export const HistoryComponent = ({ history, onHistoryClick }) => {
         ))}
       </ul>
 
+
       <div className="bottom-logo-container">
             <img src="src/assets/img1.png" />
             <img src="src/assets/img6.webp" />
+
       </div>
       <p className="bottom-text">Our partners</p>
     </div>
